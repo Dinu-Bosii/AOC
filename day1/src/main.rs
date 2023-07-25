57ccfcc27135c1bfdf291fdc740e5acfd9ea3a05
Dinu-Bosii <knsverts@gmail.com>
2023-07-25 20:34:59 +0100

    Day 1: Imperative Approach

---
+++ day1/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let file = File::open("input.txt");
+    let input = BufReader::new(file.unwrap());
+    
+    let mut best: u32 = 0;
+    let mut curr: u32 = 0;
+    
+    for line in input.lines() {
+        if let Ok(line) = line {
+            if let Ok(number) = line.trim().parse::<u32>() {
+                curr += number;    
+            }
+            else if line.trim().is_empty() {
+                if curr > best {
+                    best = curr;
+                }
+                curr = 0;
+            }
+
+        }
+    }
+    println!("The best is {best}");
+}
+
