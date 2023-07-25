c1a9b721eac02d48751d99db9dc083849c07379d
Dinu-Bosii <knsverts@gmail.com>
2023-07-25 21:10:04 +0100

    Add Day1 Part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     
     let mut best: u32 = 0;
     let mut curr: u32 = 0;
-    
+    let mut best2: u32 = 0;
+    let mut best3: u32 = 0;
     for line in input.lines() {
         if let Ok(line) = line {
             if let Ok(number) = line.trim().parse::<u32>() {
@@ -15,6 +16,8 @@ fn main() {
             }
             else if line.trim().is_empty() {
                 if curr > best {
+                    best3 = best2;
+                    best2 = best;
                     best = curr;
                 }
                 curr = 0;
@@ -23,5 +26,8 @@ fn main() {
         }
     }
     println!("The best is {best}");
+    println!("Top 3 is {}", best + best2 + best3);
+
+
 }
 
