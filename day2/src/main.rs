6fce1ad993a5760c03fbc253d1db21a5d5e9d1e1
Dinu-Bosii <knsverts@gmail.com>
2023-07-28 01:28:08 +0100

    Add Day2 Part2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -10,9 +10,17 @@ fn main() {
                           .map(|letters| get_score((letters[0], letters[1])))
                           .sum();
     println!("Total score: {}", score);
+
+    let score2: u32 = input.chars()
+                          .filter(|c| !c.is_whitespace())
+                          .collect::<Vec<char>>() 
+                          .chunks(2) 
+                          .map(|letters| get_score2((letters[0], letters[1])))
+                          .sum();
+    println!("Total score2: {}", score2);
 }
 
-
+//Part1
 fn get_score(letters: (char, char)) -> u32 {
     match letters {
         ('A', 'Y') => 6 + 2,
@@ -24,8 +32,23 @@ fn get_score(letters: (char, char)) -> u32 {
         (_, 'X') => 1,
         (_, 'Y') => 2,
         (_, 'Z') => 3,
-        _ => 1
+        _ => 0
     } 
 }
 
-// a,x - rock(+2), b,y - paper(+2), c,z - scissors(+3)
+//Part2 
+fn get_score2(letters: (char, char)) -> u32 {
+    match letters {
+        ('A', 'Y') => 3 + 1,
+        ('A', 'Z') => 6 + 2,
+        ('A', 'X') => 0 + 3,
+        ('B', 'Y') => 3 + 2,
+        ('B', 'Z') => 6 + 3,
+        ('B', 'X') => 0 + 1,
+        ('C', 'Y') => 3 + 3,
+        ('C', 'Z') => 6 + 1,
+        ('C', 'X') => 0 + 2,
+        _ => 0
+    } 
+}
+
