bc163dc0387e8b2a329edf6fb88647937583a7e5
Dinu-Bosii <knsverts@gmail.com>
2023-08-12 22:23:30 +0100

    Add Day4: Part2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
     let input: String = std::fs::read_to_string("input.txt").unwrap();
 
-    let total_overlap: u32 = input
+    //Part1
+    let total_full_overlap: u32 = input
         .lines()
         .filter(|line| !line.is_empty())
         .map(|line| {
@@ -27,7 +28,34 @@ fn main() {
         })
         .sum();
         
-    println!("Total overlapping sections: {}", total_overlap);    
+    println!("Total fully overlapping sections: {}", total_full_overlap);
+    
+    //Part2 
+    let total_overlap: u32 = input
+        .lines()
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            let assignments: Vec<&str> = line.split(',').collect();
+            let section1: Vec<&str> = assignments[0].split('-').collect();
+            let section2: Vec<&str> = assignments[1].split('-').collect();
+            let s1 = section1[0].parse::<u32>().unwrap();
+            let e1 = section1[1].parse::<u32>().unwrap();
+            let s2 = section2[0].parse::<u32>().unwrap();
+            let e2 = section2[1].parse::<u32>().unwrap();
+            
+            if e1 <= e2 && e1 >= s2 {
+                return 1;
+            }
+
+            if e1 >= e2 && e2 >= s1 {
+                return 1;
+            }
+            0
+            
+        })
+        .sum();
+        
+    println!("Total overlapping sections: {}", total_overlap);
 }
 
 
